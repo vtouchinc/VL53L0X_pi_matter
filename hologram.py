126af59df8816180f0d5eadfa05aa7c12611aab7
김윤수 <ing03201@gmail.com>
2023-12-13 14:41:34 +0900

    toggle change

--- hologram.py
+++ hologram.py
@@ -7,7 +7,12 @@ except ImportError:
     print("Error importing ST_VL6180X.VL6180X!")
     exit()
 
-
+DISTANCE_QUEUE_SIZE = 10
+PUSH_PULL_INTERVAL_CONSTANT = 60
+ATTACHABLE_DISTANCE = 150
+TOGGLE_INTERVAL_CONSTANT = 10
+TOGGLE_INTERVAL = 5
+TOF_READ_PERIOD = 0.005
 
 
 
@@ -23,18 +28,16 @@ def main():
     tof_sensor = VL6180X(address=tof_address, debug=debug)
     # apply pre calibrated offset
     tof_sensor.set_range_offset(23)
-    print("Range offset set to: {:d}".format(tof_sensor.get_range_offset()))
+    print(f"Range offset set to: {tof_sensor.get_range_offset()}")
     # setup ToF ranging/ALS sensor
     tof_sensor.get_identification()
     if tof_sensor.idModel != 0xB4:
-        print("Not a valid sensor id: {:X}".format(tof_sensor.idModel))
+        print(f"Not a valid sensor id: {tof_sensor.idModel}")
     else:
-        print("Sensor model: {:X}".format(tof_sensor.idModel))
-        print("Sensor model rev.: {:d}.{:d}"
-            .format(tof_sensor.idModelRevMajor, tof_sensor.idModelRevMinor))
-        print("Sensor module rev.: {:d}.{:d}"
-            .format(tof_sensor.idModuleRevMajor, tof_sensor.idModuleRevMinor))
-        print("Sensor date/time: {:X}/{:X}".format(tof_sensor.idDate, tof_sensor.idTime))
+        print(f"Sensor model: {tof_sensor.idModel}")
+        print(f"Sensor model rev.: {tof_sensor.idModelRevMajor}.{tof_sensor.idModelRevMinor}")
+        print(f"Sensor module rev.: {tof_sensor.idModuleRevMajor}.{tof_sensor.idModuleRevMinor}")
+        print(f"Sensor date/time: {tof_sensor.idDate}/{tof_sensor.idTime}")
     tof_sensor.default_settings()
     sleep(1)
 
@@ -47,34 +50,42 @@ def main():
             # print(f"Measured distance is :{mmDetectedDistance} mm" )
             frameList.append(mmDetectedDistance)
             check_click(frameList)
-            sleep(0.01)
+            sleep(TOF_READ_PERIOD)
     except KeyboardInterrupt:
         print("\nquit")
 
 
 def check_click(distanceList : list):
-    if(len(distanceList) >= 10):
+    if(len(distanceList) >= DISTANCE_QUEUE_SIZE):
         maxDistance = max(distanceList)
         minDistance = min(distanceList)
         distanceInterval = maxDistance - minDistance
         maxIndex = distanceList.index(maxDistance)
         minIndex = distanceList.index(minDistance)
         indexInterval = maxIndex - minIndex
-        if(distanceInterval > 60):
+        if(distanceInterval > PUSH_PULL_INTERVAL_CONSTANT):
             if( maxIndex > minIndex):
                 ## Rising edge
-                print("pull")
-                # push()
+                pull()
             else:
                 ## falling edge
-                print("push")
-                # pull()
+                push()
             distanceList.clear()
-        elif(maxDistance < 180):
-            if(indexInterval > 5):
-                print("toggle")
+        elif(maxDistance < ATTACHABLE_DISTANCE):
+            if(distanceInterval > TOGGLE_INTERVAL_CONSTANT):
+                toggle()
             # toggle()
             distanceList.clear()
+
+def push():
+    # buzzer()
+    print("push")
+
+def pull():
+    print("pull")
+
+def toggle():
+    print("toggle")
 
 if __name__== "__main__":
     main()
